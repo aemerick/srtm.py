7dd35629f0cc591f9b4581c0e5f635bb8f0bc84a
Andrew Emerick <aemerick11@gmail.com>
2020-10-19 23:06:07 -0700

    typo

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from setuprools import setup
+from setuptools import setup
 
 setup(
     name = 'SRTM.py',
